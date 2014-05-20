178f109d04c0bb8333df864be9b49befcf816d21
Christian Höltje <docwhat@gerf.org>
2014-05-20 10:30:16 -0400

    improvements

--- jobber.rb
+++ jobber.rb
@@ -1,24 +1,18 @@
 #!/usr/bin/env ruby
 
-# rubocop:disable MethodLength
-
+require 'optparse'
+require 'fileutils'
 require 'pathname'
 require 'time'
 
 # Comment
-class DateBuild
-  def number
-    # load build.xml and get XPath /*/number/text()
-  end
-end
-
-# Comment
 class Job
-  attr_reader :path, :numbers, :dates, :problems
+  attr_reader :path, :numbers, :dates, :problems, :solutions
 
   def initialize(path)
     @path = Pathname.new path
     @problems = []
+    @solutions = []
   end
 
   def builds_path
@@ -26,7 +20,6 @@ class Job
   end
 
   def scan_builds
-
     @dates = []
     @numbers = []
 
@@ -42,44 +35,51 @@ class Job
 
     @numbers.sort!
     @dates.sort!
-
-    # p @dates
-    # p @numbers
   end
 
   def problem?
-    return false if numbers.length == 0 && dates.length == 0
+    tests = methods.select { |m| m.to_s.start_with? 'test_'  }
+    tests.each do |m|
+      send m
+    end
 
-    test_next_build_number
-    test_broken_number_link
-    test_numbers_are_not_files
-    test_last_links
-    test_dates_for_numbers_are_in_order
-
-    return problems.length > 0
+    problems.length > 0
     # assert dates.map(&:number).length == dates.map(&:number).uniq.length
-    # assert dates.length == numbers.length
-    # @date_bucket.length != @number_bucket.length
   end
 
   def next_build_number
     @next_build_number ||= Integer((path + 'nextBuildNumber').read)
   end
 
-  def problem message
+  def problem(message)
     @problems << message
   end
 
-  def test_last_links
+  def solution(&block)
+    @solutions << block
+  end
+
+  def test_is_a_job_directory
+    unless (path + 'config.xml').exist?
+      problem "#{path} is not a job directory; 'config.xml' is missing."
+    end
+  end
+
+  def test_last_links_should_be_links
     links = %w(lastFailedBuild lastStableBuild lastSuccessfulBuild lastUnstableBuild lastUnsuccessfulBuild)
 
-    links.map { |l| builds_path + l }.each do |path|
-      problem "#{path} should be a symlink but isn't." if ! path.symlink? && path.exist?
+    links.map { |l| builds_path + l }.select { |p| !p.symlink? && p.exist? }.each do |path|
+      problem "#{path} should be a symlink but isn't."
+
+      solution do
+        path.rmtree if path.directory?
+        path.unlink if path.exist?
+        File.symlink '-1', path.to_s
+      end
     end
   end
 
   def test_numbers_are_not_files
-    bad = []
     numbers.each do |n|
       link = builds_path + n.to_s
       problem "The number link #{link} is not a symlink!" unless link.symlink?
@@ -88,37 +88,45 @@ class Job
 
   def test_broken_number_link
     # numbers.map(&:link?).reduce(true) { |a, e| a && e }
-    bad = []
-    numbers.each do |n|
-      link = builds_path + n.to_s
-      problem "The number link #{link} is broken." unless link.exist?
+    numbers.map { |n| builds_path + n.to_s }.reject(&:exist?).each do |link|
+      problem "The number link #{link} is broken."
+      solution { link.unlink if link.exist? }
     end
   end
 
   def test_dates_for_numbers_are_in_order
-    # numbers.sort.each { |n| assert n.date > (prev_n).date }
-    # numbers_dates = numbers.map { |n| builds_path + n.to_s }.select(&:symlink?).select(&:exist?).map do |p|
-    #   p.readlink.basename.to_s.match(/^(\d{4}-\d{2}-\d{2})_(\d{2})-(\d{2})-(\d{2})$/)
-    # end.map do |m|
-    #   Time.parse "#{m[1]} #{m[2]}:#{m[3]}:#{m[4]}"
-    # end
-    numbers_dates = numbers.map { |n| builds_path + n.to_s }.select(&:symlink?).select(&:exist?).map do |p|
-      [p, p.readlink.basename.to_s.match(/^(\d{4}-\d{2}-\d{2})_(\d{2})-(\d{2})-(\d{2})$/)]
-    end.map do |p, m|
-      [p, Time.parse("#{m[1]} #{m[2]}:#{m[3]}:#{m[4]}")]
+    valid_numbers_paths = numbers.map { |n| builds_path + n.to_s }.select(&:symlink?).select(&:exist?)
+
+    valid_numbers_dates = valid_numbers_paths.map do |path|
+      m = path.readlink.basename.to_s.match(/^(\d{4}-\d{2}-\d{2})_(\d{2})-(\d{2})-(\d{2})$/)
+      Time.parse("#{m[1]} #{m[2]}:#{m[3]}:#{m[4]}")
     end
 
-    numbers_dates.each_with_index do |path_and_date, i|
+    numbers_paths_and_dates = valid_numbers_paths.zip(valid_numbers_dates)
+
+    numbers_paths_and_dates.each_with_index do |path_and_date, i|
       path, date = path_and_date
-      problem "The link #{path} -> #{path.readlink} is out of order." if date > numbers_dates.map(&:last)[i..-1].min
+      if date > numbers_paths_and_dates.map(&:last)[i..-1].min
+        problem "The link #{path} -> #{path.readlink} is out of order."
+      end
     end
     # if numbers_dates != numbers_dates.sort
     #   problem "The number links are out of order!"
     # end
   end
 
+  def test_numbers_equals_dates
+    # assert dates.length == numbers.length
+    if dates.length > numbers.length
+      problem 'There are more dates files than numbers files.'
+    elsif dates.length < numbers.length
+      problem 'There are more numbers files than dates files.'
+    end
+  end
+
   def test_next_build_number
     # assert next_build_number == numbers.sort.last + 1
+    return if dates.empty? && numbers.empty?
     given = next_build_number
     expected = numbers.last + 1
     if given < expected
@@ -128,21 +136,42 @@ class Job
 end
 
 if $PROGRAM_NAME == __FILE__
+  $stdout.sync = true # unbuffered output
+
+  mode = :seek
+
+  OptionParser.new do |opts|
+    opts.banner = 'Usage: jobber.rb [options]'
+
+    opts.on('-s', '--solve', 'Try to automagically solve the problems') do
+      mode = :destroy
+    end
+  end.parse!
+
   if ARGV.map { |p| File.directory? p }.reduce(true) { |a, e| a && e }
+    print 'Scanning: '
     all_problems = {}
+    all_solutions = {}
     ARGV.each do |path|
-      puts " ** #{path} **"
       job = Job.new path
       job.scan_builds
+      # p job
       # p job.next_build_number
 
-      all_problems[File.basename path] = job.problems if job.problem?
+      if job.problem?
+        all_problems[File.basename path] = job.problems
+        all_solutions[File.basename path] = job.solutions unless job.solutions.empty?
+        print '!'
+      else
+        print '.'
+      end
     end
+    puts
 
     if all_problems.length > 0
       count = 0
       puts
-      puts "**** PROBLEMS ****"
+      puts '**** PROBLEMS ****'
       all_problems.keys.sort.each do |key|
         puts "#{key}:"
         all_problems[key].each do |problem|
@@ -153,8 +182,18 @@ if $PROGRAM_NAME == __FILE__
 
       puts
       puts "Found #{count} problems."
+
+      if :destroy == mode
+        puts
+        puts '**** SOLUTIONS ****'
+        all_solutions.keys.sort.each do |key|
+          print "Fixing #{key}: "
+          all_solutions[key].each { |solution| solution.call }
+          puts 'done.'
+        end
+      end
     else
-      puts "No problems!"
+      puts 'No problems!'
     end
   else
     fail "You need to give me a 'build' style directory."
